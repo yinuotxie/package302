42e4b4e70445edce6d57edf1cc80bc1f0662774a
Travis Xie <yinuox@uw.edu>
2020-03-08 04:25:20 -0700

    Add tests for my_rf_cv

--- tests/testthat/test-my_rf_cv.R
+++ tests/testthat/test-my_rf_cv.R
@@ -1,3 +1,25 @@
+# data for test
+data("iris")
+# input data frame
+train <- iris[, -5]
+# true class
+cl <- iris[, 5]
+test_that("my_rf_cv works", {
+  # cv_err vector that stores cv_err for 100 trials
+  cv_err <- rep(NA, 100)
+  # mean mse for 100 trials
+  for (i in 1:100) {
+    cv_err[i] <- my_rf_cv(5)
+  }
+  mean <- mean(cv_err)
+  sd <- sd(cv_err)
+  my_result <- my_rf_cv(5)
+  # I am satisified with 95 confidence interval, aka, my_result is 2sds aways
+  # from the mean
+  away <- (my_result - mean) / sd
+  expect_true(abs(away) <= 2)
+})
+
 test_that("non numeric input for k throws an error", {
   expect_error(my_rf_cv("a"))
 })
