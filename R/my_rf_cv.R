66359f90f5a779a0a0710780156eabcc3c372121
Travis Xie <yinuox@uw.edu>
2020-03-07 19:44:46 -0800

    Fix errors

--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -6,7 +6,7 @@
 #' @param k Number of folds.
 #' @keywords cross-validation
 #'
-#' @return A numeric with the cross-validation error
+#' @return A numeric with the cross-validation error.
 #'
 #' @import class
 #' @import randomForest
@@ -22,11 +22,10 @@
 #'
 #' @export
 my_rf_cv <- function(k) {
-  data("my_gapminder", envir = environment())
-  train <- my_gapminder %>% select(lifeExp, gdpPercap)
   # randomly assigns observations to folds 1,…,k with equal probability
+  train <- my_gapminder %>% select(lifeExp, gdpPercap)
   fold <- sample(rep(1:k, length = nrow(train)))
-  data <- train %>% mutate(fold = fold)
+  my_data <- train %>% mutate(fold = fold)
 
   # evaluate MSE, the average squared difference between predicted
   # Sepal.Length and true Sepal.Length.
@@ -34,14 +33,14 @@ my_rf_cv <- function(k) {
   # loop through the folds
   for (i in 1:k) {
     # get the training data
-    data_train <- data %>% filter(fold != i) %>% select(-contains("fold"))
+    data_train <- my_data %>% filter(fold != i) %>% select(-fold)
     # get the test data
-    data_test <- data %>% filter(fold == i) %>% select(-contains("fold"))
+    data_test <- my_data %>% filter(fold == i) %>% select(-fold)
     # get the model
     model <- randomForest(lifeExp ~ gdpPercap, data = data_train, ntree = 100)
     # predict the life expentance of test data
-    # the first column of data_test is Sepal.Length
-    pred <- predict(model, data_test$gdpPercap)
+    # the second column is gdpPercap
+    pred <- predict(model, data_test[, 2])
     # evaluate MSE, the average squared difference between predicted
     # lifeExp and true lifeExp
     mse[i] <- mean((pred - data_test$lifeExp)^2)
